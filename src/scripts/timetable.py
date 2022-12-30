18156c1011e62fcfd302e16844fe7be67a407240
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-12-30 17:05:50 +0900

    feat: 전체 노선의 전철 시간표 추가

--- src/scripts/timetable.py
+++ src/scripts/timetable.py
@@ -1,4 +1,3 @@
-import asyncio
 import csv
 
 from aiohttp import ClientTimeout, ClientSession
@@ -8,59 +7,57 @@ from sqlalchemy.orm import Session
 from models import SubwayTimetable, SubwayRouteStation
 
 
-async def get_timetable_data(db_session: Session, route_name: str, route_id: int) -> None:
+async def get_timetable_data(db_session: Session, route_id: int) -> None:
     timetable_items: list[dict] = []
-    terminal_station_items: list[str] = []
-    terminal_station_id: dict[str, str] = {}
+    station_list: list[str] = []
+    station_id_dict: dict[str, str] = {}
     station_name_dict: dict[str, str] = {
-        "신인천": "인천",
-        "한성대": "한성대입구",
+        "신인천": "인천", "한성대": "한성대입구", "경마공": "경마공원", "강남구": "강남구청",
+        "남동인": "남동인더스파크", "매탄권": "매탄권선", "신길온": "신길온천", "4동운": "동대문역사문화공원",
+        "수원시": "수원시청", "신수원": "수원", "4동대": "동대문", "로데오": "압구정로데오",
+        "소래포": "소래포구", "총신대": "이수", "미아4": "미아사거리", "인천논": "인천논현",
+        "구룡역": "구룡", "별가람": "별내별가람", "성신여": "성신여대입구", "4창동": "창동",
+        "4충무": "충무로", "평촌동": "평촌", "숙대입": "숙대입구", "과천청": "과천정부청사",
+        "4이촌": "이촌", "4서울": "서울역",
     }
+    exclude_station_list: list[str] = ["별가람", "오남", "진접"]
+    async with ClientSession(timeout=ClientTimeout(total=10)) as session:
+        url = f"https://raw.githubusercontent.com/hyuabot-developers/hyuabot-subway-timetable/main/{route_id}.csv"
+        async with session.get(url) as response:
+            if response.status != 200:
+                raise RuntimeError("Failed to get timetable data")
+            reader = csv.reader((await response.text()).splitlines())
+            for station, weekdays, heading, terminal, departure_time in reader:
+                if station in exclude_station_list or terminal in exclude_station_list:
+                    continue
+                if station in station_name_dict:
+                    station = station_name_dict[station]
+                if terminal in station_name_dict:
+                    terminal = station_name_dict[terminal]
+                station_list.extend([station, terminal])
+                timetable_items.append({
+                    "station_name": station,
+                    "up_down_type": heading,
+                    "weekday": weekdays,
+                    "departure_time": departure_time,
+                    "terminal_station_name": terminal,
+                })
 
-    station_query_statement = select(SubwayRouteStation.station_id).where(
-        and_(SubwayRouteStation.station_name == "한대앞", SubwayRouteStation.route_id == route_id))
-    start_station_id = ""
-    for row in db_session.execute(station_query_statement):
-        start_station_id = row[0]
-        break
-    if not start_station_id:
-        raise RuntimeError("Failed to get start station id")
-    for weekday in ["weekdays", "weekends"]:
-        for heading in ["up", "down"]:
-            url = ""
-            try:
-                url = "https://raw.githubusercontent.com/hyuabot-developers/" \
-                      f"hyuabot-subway-timetable/main/{route_name}/{weekday}/{heading}.csv"
-                timeout = ClientTimeout(total=3.0)
-                async with ClientSession(timeout=timeout) as session:
-                    async with session.get(url) as response:
-                        reader = csv.reader((await response.text()).splitlines())
-                        for terminal_station_name, departure_time in reader:
-                            if terminal_station_name in station_name_dict.keys():
-                                terminal_station_name = station_name_dict[terminal_station_name]
-                            timetable_items.append({
-                                "station_id": start_station_id,
-                                "weekday": weekday,
-                                "up_down_type": heading,
-                                "departure_time": departure_time,
-                                "terminal_station": terminal_station_name,
-                            })
-                            terminal_station_items.append(terminal_station_name)
-            except asyncio.exceptions.TimeoutError:
-                print("TimeoutError")
-            except AttributeError:
-                print("AttributeError", url)
-    for station_name in list(set(terminal_station_items)):
-        station_item_query = select(SubwayRouteStation.station_id).where(
-            and_(SubwayRouteStation.station_name == station_name, SubwayRouteStation.route_id == route_id))
-        station_query = db_session.execute(station_item_query).fetchone()
-        if station_query is None:
-            raise RuntimeError("Failed to get station id")
-        station_id = station_query[0]
-        terminal_station_id[station_name] = station_id
+    station_list = list(set(station_list))
+    for station in station_list:
+        station_query_statement = select(SubwayRouteStation.station_id).where(
+            and_(SubwayRouteStation.station_name == station, SubwayRouteStation.route_id == route_id))
+        station_id = ""
+        for row in db_session.execute(station_query_statement):
+            station_id = row[0]
+            break
+        if station_id == "":
+            raise RuntimeError(f"Failed to get station id for {station}")
+        station_id_dict[station] = station_id
     for timetable_item in timetable_items:
-        timetable_item["station_id"] = start_station_id
-        timetable_item["terminal_station_id"] = terminal_station_id[timetable_item["terminal_station"]]
-        del timetable_item["terminal_station"]
+        timetable_item["station_id"] = station_id_dict[timetable_item["station_name"]]
+        del timetable_item["station_name"]
+        timetable_item["terminal_station_id"] = station_id_dict[timetable_item["terminal_station_name"]]
+        del timetable_item["terminal_station_name"]
     db_session.execute(insert(SubwayTimetable).values(timetable_items))
     db_session.commit()
